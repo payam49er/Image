4d8b68f8202f349d4c0912ba71a50247898c925b
payam49er <payam49er@gmail.com>
2012-11-21 17:46:32 -0500

    most recent

--- measureFocus/measureFocus/Program.cs
+++ measureFocus/measureFocus/Program.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Drawing.Drawing2D;
+
 namespace ImageFocus
 {
     class Focus
@@ -17,14 +18,15 @@ namespace ImageFocus
         //this method takes an image as parameter
         private Bitmap ConvertToBitmap(Image source)
         {
-            Bitmap bitmap = new Bitmap(source);
-            return bitmap;
+            Bitmap image = new Bitmap(source);
+            return image;
         }
 
         //method to read bitmap image file and convert it to grayscale 
-        public Bitmap ConvertToGrayScale(Bitmap source)
+        public double GetFValue(Bitmap source)
         {
             Bitmap bm = new Bitmap(source.Width, source.Height);
+            double brenner = 0.0;
             for (int y = 0; y < bm.Height; y++)
             {
                 for (int x = 0; x < bm.Width; x++)
@@ -32,44 +34,68 @@ namespace ImageFocus
                     Color c = source.GetPixel(x, y);
                     int luma = (int)(c.R * 0.3 + c.G * 0.59 + c.B * 0.11);
                     bm.SetPixel(x, y, Color.FromArgb(luma, luma, luma));
+                    var p = bm.GetPixel(x, y);
+                    var p1 = bm.GetPixel(x + 2, y);
+                    var firstValue =(double) p.B;
+                    var secondValue =(double) p1.G;
+
+                    brenner += ((secondValue - firstValue) * (secondValue - firstValue));
+
+                    
                 }
             }
-            return bm;
+            return brenner;
         }
 
-        public Color[][] GetImageMatrix(Image source)
-        {
-            Bitmap bm = new Bitmap(source);
-            int Hsize = bm.Width;
-            int Vsize = bm.Height;
+        
 
-            Color[][] imageMatrix = new Color[Hsize][];
-            for (int i = 0; i < Hsize; i++)
-            {
-                imageMatrix[i] = new Color[Vsize];
-                for (int j = 0; j < Vsize; j++)
-                {
-                    Color c = bm.GetPixel(j, j);
-                    int luma = (int)(c.R * 0.3 + c.G * 0.59 + c.B * 0.11);
-                    bm.SetPixel(i, j, Color.FromArgb(luma, luma, luma));
-                    imageMatrix[i][j] = bm.GetPixel(i, j);
-                }
-            }
-            return imageMatrix;
-        }
+
+        //public double GetBrennerFocus(Image source)
+        //{
+        //    Bitmap bm = new Bitmap(source);
+        //    int Hsize = bm.Width;
+        //    int Vsize = bm.Height;
+        //    double brenner = 0;
+        //    Color[][] imageMatrix = new Color[Hsize][];
+        //    for (int i = 0; i < Hsize; i++)
+        //    {
+        //        imageMatrix[i] = new Color[Vsize];
+        //        for (int j = 0; j < Vsize; j++)
+        //        {
+        //            Color c = bm.GetPixel(j, j);
+        //            int luma = (int)(c.R * 0.3 + c.G * 0.59 + c.B * 0.11);
+        //            bm.SetPixel(i, j, Color.FromArgb(luma, luma, luma));
+        //            imageMatrix[i][j] = bm.GetPixel(i, j);
+        //         }
+        //    }
+            
+        //     for(int j=0 ; j<Hsize; j++)
+        //    {
+        //        for (int i=0; i<Vsize; i++)
+        //        {
+        //            int[] p = imageMatrix[i];
+        //            int[] p1 = imageMatrix.Getvalue(i+2,j);
+        //            double brenner += (p1-p)*(p1-p);
+                    
+                    
+        //        }
+        //    }
+
+        //   return brenner; 
+        //}
+
+             
 
         // main method that I am using just to test my methods in the class Focus
         static void Main(string[] args)
         {
+          Focus testimage = new Focus();
 
-            Focus testimage = new Focus();
+           Image myimage = Image.FromFile(@"C:\Users\payam\Desktop\focusTest.png", true);
 
-            Image myimage = Image.FromFile(@"C:\Users\payam\Desktop\focusTest.png", true);
-            
-
-            var result = testimage.GetImageMatrix(myimage);
-            // Bitmap grayimage = testimage.ConvertToGrayScale(result);
-            Console.Write(result);
+           Bitmap bitmapimage = testimage.ConvertToBitmap(myimage);
+           var FM = testimage.GetFValue(bitmapimage);
+           Console.WriteLine(FM);
             Console.ReadLine();
         }
     }
